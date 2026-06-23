ce943d304891c0ef6759abb11eff43b8bf558bcd
carneslaasaderia <carnes.laasaderia@gmail.com>
2026-06-23 12:44:26 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-// La Asadería
-// Javascript inicial
+const slides = document.querySelectorAll('.friend-slide');
 
-console.log("La Asadería cargada correctamente");
+let current = 0;
+
+function updateCarousel(){
+
+    slides.forEach(slide=>{
+        slide.classList.remove('active');
+    });
+
+    slides[current].classList.add('active');
+
+    slides[current].scrollIntoView({
+        behavior:'smooth',
+        inline:'center',
+        block:'nearest'
+    });
+
+}
+
+document.querySelector('.next')
+.addEventListener('click',()=>{
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    updateCarousel();
+
+});
+
+document.querySelector('.prev')
+.addEventListener('click',()=>{
+
+    current--;
+
+    if(current < 0){
+        current = slides.length - 1;
+    }
+
+    updateCarousel();
+
+});
+
+updateCarousel();
